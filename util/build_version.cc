472b17cfb570d65a1f98ef880548e2a81754d0eb
liguoqiang <liguoqiang@baidu.com>
2020-03-04 15:04:36 +0800

    CPD-40785 update to 6.6.4
    Change-Id: I9ddb6c2a841293ad38312e0cc6430e3e5e68c0ce

---
+++ util/build_version.cc
@@ -0,0 +1,5 @@
+// Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+#include "build_version.h"
+const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:c63077aebd447937639743e32f483e0d32b68bd3";
+const char* rocksdb_build_git_date = "rocksdb_build_git_date:2020-03-03";
+const char* rocksdb_build_compile_date = __DATE__;
