92fba05f60426d75fadc9dadc2051231ad3e4e5d
liguoqiang <liguoqiang@baidu.com>
2021-11-22 15:32:48 +0800

    v6.26.0 fix
    Change-Id: I5122cc0e155368b84376f2794856b09e306b1f8b

--- db/memtable.h
+++ db/memtable.h
@@ -149,6 +149,7 @@ class MemTable {
   // used by MemTableListVersion::ApproximateMemoryUsageExcludingLast
   size_t ArenaBlockSize() const {
     return kArenaBlockSize;
+  }
   // Returns a vector of unique random memtable entries of size 'sample_size'.
   //
   // Note: the entries are stored in the unordered_set as length-prefixed keys,
