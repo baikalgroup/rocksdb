4fc76de4d9a50e70226b08f65ab5b905aa401b49
liguoqiang <liguoqiang@baidu.com>
2021-12-14 21:23:38 +0800

    fix memleak
    Change-Id: I833f36f43672a496eb8599dd0cdae884f96ade14

--- table/block_based/block_based_table_builder.cc
+++ table/block_based/block_based_table_builder.cc
@@ -910,8 +910,7 @@ void BlockBasedTableBuilder::Add(const Slice& key, const Slice& value) {
       r->first_key_in_next_block = &key;
       Flush();
       if (r->state == Rep::State::kBuffered) {
-        bool exceeds_buffer_limit =
-            (r->buffer_limit != 0 && r->data_begin_offset > r->buffer_limit);
+        bool exceeds_buffer_limit = r->data_begin_offset > r->buffer_limit;
         bool is_cache_full = false;
 
         // Increase cache reservation for the last buffered data block
